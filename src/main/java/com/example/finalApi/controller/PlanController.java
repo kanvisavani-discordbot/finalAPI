521e91ab96861fd5491ac1b385506e0ab39bca48
Kanvi <kanvisavani123@gmail.com>
2021-10-27 02:18:21 -0400

    delete,update and get plans

--- src/main/java/com/example/finalApi/controller/PlanController.java
+++ src/main/java/com/example/finalApi/controller/PlanController.java
@@ -1,11 +1,15 @@
 package com.example.finalApi.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +31,11 @@ public class PlanController {
 		return planRepository.findAll();		
 	}
 	
+	 @GetMapping(value="/plans/{id}")
+	 public Optional<Plan> findPlan(@PathVariable Integer id) {	    
+	    return planRepository.findById(id);
+	  }
+	
 	@PostMapping(value="/create")
 	public String createPlan(@RequestBody Plan plan) {
 		plan.setId(service.getSequenceNumber(Plan.SEQUENCE_NAME));
@@ -34,4 +43,14 @@ public class PlanController {
 		return "Plan created "+insertedPlan.getName();
 	}
 	
+	@DeleteMapping(value="/plans/{id}")
+	public void deletePlan(@PathVariable Integer id) {
+	    planRepository.deleteById(id);
+	  }
+	
+	@PutMapping(value="/plan/{id}")
+	public Plan updatePlan(@RequestBody Plan newPlan, @PathVariable Integer id) {
+		newPlan.setId(id);
+	    return planRepository.save(newPlan);
+	  }	
 }
